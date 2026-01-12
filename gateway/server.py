c815dd67c13195698582eecb3ee01273eb7b148e
yuvraj-rathod-1202 <rathodyuvraj12022007@gmail.com>
2026-01-12 12:01:26 +0530

    added cors allow in gateway

--- gateway/server.py
+++ gateway/server.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes.auth import router as auth_router
 from routes.courses import router as courses_router
 from routes.users import router as users_router
@@ -7,6 +8,20 @@ from routes.policy import router as policy_router
 from routes.analytics import router as analytics_router
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(auth_router, prefix="/auth")
 app.include_router(courses_router, prefix="/courses")
 app.include_router(users_router, prefix="/courses/users")
